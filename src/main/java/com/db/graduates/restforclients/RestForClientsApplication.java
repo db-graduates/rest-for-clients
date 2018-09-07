f80b31b0bb4e8d0e8cc9eb466b24f2447da3d83c
Anastasiia Turenko <aaturenko@gmail.com>
2018-09-07 17:34:58 +0300

    add eureka

--- src/main/java/com/db/graduates/restforclients/RestForClientsApplication.java
+++ src/main/java/com/db/graduates/restforclients/RestForClientsApplication.java
@@ -4,7 +4,9 @@ import com.db.graduates.restforclients.service.ChartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class RestForClientsApplication {
 
